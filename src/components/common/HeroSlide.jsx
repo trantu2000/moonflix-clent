b1dd8df5295ce8d2f7374ffea00c0946946cbe9c
tran thanh tu <tttuab@gmail.com>
2023-01-30 00:47:59 +0700

    update

--- src/components/common/HeroSlide.jsx
+++ src/components/common/HeroSlide.jsx
@@ -1,4 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Chip,
+  Divider,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { Stack } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,8 +14,16 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { setGlobalLoading } from "../../redux/features/globalLoadingSlice";
 import genreApi from "../../api/modules/genre.api";
+import tmdbConfigs from "../../api/configs/tmdb.configs";
+import uiConfigs from "../../configs/ui.configs";
+import { Link } from "react-router-dom";
+import { routesGen } from "../../routes/routes";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import CircularRate from "./CircularRate";
+import { Autoplay } from "swiper";
 
 const HeroSlide = ({ mediaType, mediaCategory }) => {
+  const theme = useTheme();
   const dispatch = useDispatch();
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
@@ -45,19 +60,141 @@ const HeroSlide = ({ mediaType, mediaCategory }) => {
   }, [mediaType, mediaCategory, dispatch]);
 
   return (
-    <Box>
-      <Swiper>
-        {movies?.map((movie, index) => (
-          <SwiperSlide>
-            <Box>
-              <Box>
-                <Box>
-                  <Box>
-                    <Stack>
-                      <Typography></Typography>
-                    </Stack>
-                  </Box>
-                </Box>
+    <Box
+      sx={{
+        position: "relative",
+        color: "primary.contrastText",
+        "&::before": {
+          content: '""',
+          width: "100%",
+          height: "30%",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          zIndex: 2,
+          pointerEvents: "none",
+          ...uiConfigs.style.gradientBgImage[theme.palette.mode],
+        },
+      }}
+    >
+      <Swiper
+        grabCursor={true}
+        loop={true}
+        // modules={[Autoplay]}
+        style={{ width: "100%", height: "max-content" }}
+        // autoplay={{
+        //   delay: 3000,
+        //   disableOnInteraction: false
+        // }}
+      >
+        {movies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <Box
+              sx={{
+                paddingTop: {
+                  xs: "130%",
+                  sm: "80%",
+                  md: "60%",
+                  lg: "45%",
+                },
+                backgroundPosition: "top",
+                backgroundSize: "cover",
+                backgroundImage: `url(${tmdbConfigs.backdropPath(
+                  movie.backdrop_path || movie.poster_path
+                )})`,
+              }}
+            />
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                // ...uiConfigs.style.horizontalGradientBgImage[
+                //   theme.palette.mode
+                // ],
+              }}
+            />
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                paddingX: { sm: "10px", md: "5rem", lg: "10rem" },
+              }}
+            >
+              <Box
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  paddingX: "30px",
+                  color: "text.primary",
+                  width: { sm: "unset", md: "30%", lg: "40%" },
+                }}
+              >
+                <Stack spacing={4} direction="column">
+                  {/* title */}
+                  <Typography
+                    variant="h4"
+                    fontSize={{ xs: "2rem", md: "2rem", lg: "4rem" }}
+                    fontWeight="700"
+                    sx={{
+                      ...uiConfigs.style.typoLines(2, "left"),
+                    }}
+                  >
+                    {movie.title || movie.name}
+                  </Typography>
+                  {/* title */}
+
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    {/* rating */}
+                    <CircularRate value={movie.vote_average} />
+                    {/* rating */}
+
+                    <Divider orientation="vertical" />
+                    {/* genres */}
+                    {[...movie.genre_ids].splice(0, 2).map((genreId, index) => (
+                      <Chip
+                        variant="filled"
+                        color="primary"
+                        key={index}
+                        label={
+                          genres.find((e) => e.id === genreId) &&
+                          genres.find((e) => e.id === genreId).name
+                        }
+                      />
+                    ))}
+                    {/* genres */}
+                  </Stack>
+
+                  {/* overview */}
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      ...uiConfigs.style.typoLines(3),
+                    }}
+                  >
+                    {movie.overview}
+                  </Typography>
+                  {/* overview */}
+
+                  {/* buttons */}
+                  <Button
+                    variant="contained"
+                    size="large"
+                    startIcon={<PlayArrowIcon />}
+                    component={Link}
+                    to={routesGen.mediaDetail(mediaType, movie.id)}
+                    sx={{ width: "max-content" }}
+                  >
+                    watch now
+                  </Button>
+                  {/* buttons */}
+                </Stack>
               </Box>
             </Box>
           </SwiperSlide>
--- src/components/common/PageWrapper.jsx
+++ src/components/common/PageWrapper.jsx
@@ -4,13 +4,16 @@ import { setAppState } from "../../redux/features/appStateSlice";
 
 const PageWrapper = ({ state, children }) => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
     dispatch(setAppState(state));
   }, [state, dispatch]);
+
   return children;
 };
 
