4a6cd13f9b3ad9299d4bb76964d8d7db6c8c7e11
tran thanh tu <tttuab@gmail.com>
2023-01-30 01:00:17 +0700

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import HeroSlide from "../components/common/HeroSlide";
 import tmdbConfigs from "../api/configs/tmdb.configs";
+import { Box } from "@mui/material";
+import Container from "../components/common/Container";
+import uiConfigs from "../configs/ui.configs";
+import MediaSlide from "../components/common/MediaSlide";
 
 const HomePage = () => {
   return (
     <>
-      <HeroSlide
+      {/* <HeroSlide
         mediaType={tmdbConfigs.mediaType.movie}
         mediaCategory={tmdbConfigs.mediaCategory.popular}
-      />
+      /> */}
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+        <Container header="popular movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+      </Box>
     </>
   );
 };
