5c2f56e14b345f5afb7c44cba3aabc428913c2db
tran thanh tu <tttuab@gmail.com>
2023-01-30 09:49:25 +0700

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,14 +13,35 @@ const HomePage = () => {
         mediaType={tmdbConfigs.mediaType.movie}
         mediaCategory={tmdbConfigs.mediaCategory.popular}
       />
-      {/* <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
         <Container header="popular movies">
           <MediaSlide
             mediaType={tmdbConfigs.mediaType.movie}
             mediaCategory={tmdbConfigs.mediaCategory.popular}
           />
         </Container>
-      </Box> */}
+
+        <Container header="popular series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+
+        <Container header="top rated movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+
+        <Container header="top rated series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+      </Box>
     </>
   );
 };
