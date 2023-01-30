957fbf6f88e8538c247a506efa270d2301d246b1
tran thanh tu <tttuab@gmail.com>
2023-01-30 10:56:03 +0700

    update

--- src/pages/MediaSearch.jsx
+++ src/pages/MediaSearch.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { LoadingButton } from "@mui/lab";
+import { Box, Button, Stack, TextField, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+const mediaTypes = ["movie", "tv", "people"];
 
 const MediaSearch = () => {
+  const [medias, setMedias] = useState([]);
   return (
-    <div>
-      MediaSearch
-    </div>
-  )
-}
+    <>
+      <Toolbar />;
+      <Box>
+        <Stack>
+          {mediaTypes.map((item, index) => (
+            <Button>{item}</Button>
+          ))}
+        </Stack>
+        <TextField
+          color="success"
+          placeholder="Search MoonFlix"
+          sx={{ width: "100%" }}
+          autoFocus
+        />
 
-export default MediaSearch
+        {medias.length > 0 && <LoadingButton>load more</LoadingButton>}
+      </Box>
+    </>
+  );
+};
+
+export default MediaSearch;
