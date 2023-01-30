71274b55a98819fe2746192ba6ba362997788613
tran thanh tu <tttuab@gmail.com>
2023-01-30 18:06:48 +0700

    update

---
+++ src/components/common/CastSlide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CastSlide = () => {
+  return (
+    <div>
+      CastSlide
+    </div>
+  )
+}
+
+export default CastSlide
--- src/pages/MediaDetail.jsx
+++ src/pages/MediaDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ImageHeader from "../components/common/ImageHeader";
 import tmdbConfigs from "../api/configs/tmdb.configs";
 import { setGlobalLoading } from "../redux/features/globalLoadingSlice";
@@ -6,27 +6,46 @@ import { useDispatch } from "react-redux";
 import mediaApi from "../api/modules/media.api";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Chip, Divider, Stack, Typography } from "@mui/material";
 import uiConfigs from "../configs/ui.configs";
+import CircularRate from "../components/common/CircularRate";
+import { LoadingButton } from "@mui/lab";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import Container from "../components/common/Container";
+import CastSlide from "../components/common/CastSlide";
 
 const MediaDetail = () => {
   const { mediaType, mediaId } = useParams();
   const [media, setMedia] = useState();
-  console.log(media);
+  // console.log(media);
+  const [genres, setGenres] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
   const dispatch = useDispatch();
+
+  const videoRef = useRef(null);
 
   useEffect(() => {
     window.scrollTo(0, 0);
     const getMedia = async () => {
       dispatch(setGlobalLoading(true));
-      const { reponse, err } = await mediaApi.getDetail({ mediaType, mediaId });
+      const { response, err } = await mediaApi.getDetail({
+        mediaType,
+        mediaId,
+      });
       dispatch(setGlobalLoading(false));
 
-      if (reponse) {
-        setMedia(reponse);
+      if (response) {
+        setMedia(response);
+        // setIsFavorite(response.isFavorite);
+        setGenres(response.genres.splice(0, 2));
       }
+
       if (err) toast.error(err.message);
     };
+
     getMedia();
   }, [mediaType, mediaId, dispatch]);
 
@@ -97,6 +116,74 @@ const MediaDetail = () => {
                   }`}
                 </Typography>
                 {/* title */}
+
+                {/* rate and genres */}
+                <Stack direction="row" spacing={1} alignItems="center">
+                  {/* rate */}
+                  <CircularRate value={media.vote_average} />
+                  {/* rate */}
+
+                  <Divider orientation="vertical" />
+
+                  {/* genres */}
+                  {genres.map((genre, index) => (
+                    <Chip
+                      label={genre.name}
+                      variant="filled"
+                      color="primary"
+                      key={index}
+                    />
+                  ))}
+                  {/* genres */}
+                </Stack>
+                {/* rate and genres */}
+
+                {/* overview */}
+                <Typography
+                  variant="body1"
+                  sx={{ ...uiConfigs.style.typoLines(5) }}
+                >
+                  {media.overview}
+                </Typography>
+                {/* overview */}
+
+                {/* buttons */}
+                <Stack direction="row" spacing={1}>
+                  <LoadingButton
+                    variant="text"
+                    sx={{
+                      width: "max-content",
+                      "& .MuiButon-starIcon": { marginRight: "0" },
+                    }}
+                    size="large"
+                    startIcon={
+                      isFavorite ? (
+                        <FavoriteIcon />
+                      ) : (
+                        <FavoriteBorderOutlinedIcon />
+                      )
+                    }
+                    loadingPosition="start"
+                    // loading={onRequest}
+                    // onClick={onFavoriteClick}
+                  />
+                  <Button
+                    variant="contained"
+                    sx={{ width: "max-content" }}
+                    size="large"
+                    startIcon={<PlayArrowIcon />}
+                    onClick={() => videoRef.current.scrollIntoView()}
+                  >
+                    watch now
+                  </Button>
+                </Stack>
+                {/* buttons */}
+
+                {/* cast */}
+                <Container header="Cast">
+                  <CastSlide casts={media.credits.cast} />
+                </Container>
+                {/* cast */}
               </Stack>
             </Box>
 
