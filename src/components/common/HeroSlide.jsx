de399d611f18d0771b304d2d2e0e39113f84b8f3
tran thanh tu <tttuab@gmail.com>
2023-01-30 10:27:09 +0700

    update

--- src/components/common/HeroSlide.jsx
+++ src/components/common/HeroSlide.jsx
@@ -61,21 +61,21 @@ const HeroSlide = ({ mediaType, mediaCategory }) => {
 
   return (
     <Box
-    // sx={{
-    //   position: "relative",
-    //   color: "primary.contrastText",
-    //   "&::before": {
-    //     content: '""',
-    //     width: "100%",
-    //     height: "30%",
-    //     position: "absolute",
-    //     bottom: 0,
-    //     left: 0,
-    //     zIndex: 2,
-    //     pointerEvents: "none",
-    //     ...uiConfigs.style.gradientBgImage[theme.palette.mode],
-    //   },
-    // }}
+      sx={{
+        position: "relative",
+        color: "primary.contrastText",
+        "&::before": {
+          content: '""',
+          width: "100%",
+          height: "30%",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          zIndex: 2,
+          pointerEvents: "none",
+          ...uiConfigs.style.gradientBgImage[theme.palette.mode],
+        },
+      }}
     >
       <Swiper
         grabCursor={true}
