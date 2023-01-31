de7c9de90029ae0459350e5195e163a9079e7064
tran thanh tu <tttuab@gmail.com>
2023-01-31 15:19:57 +0700

    update

--- src/components/common/MediaReview.jsx
+++ src/components/common/MediaReview.jsx
@@ -14,8 +14,23 @@ import dayjs from "dayjs";
 import { useSelector } from "react-redux";
 import { LoadingButton } from "@mui/lab";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import reviewApi from "../../api/modules/review.api";
+import { toast } from "react-toastify";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const ReviewItem = ({ review, onRemoved }) => {
+  const { user } = useSelector((state) => state.user);
+  const [onRequest, setOnRequest] = useState(false);
+
+  const onRemove = async () => {
+    if (onRequest) return;
+    setOnRequest(true);
+
+    const { response, err } = await reviewApi.remove({ reviewId: review.id });
+
+    if (err) toast.error(err.message);
+    if (response) onRemoved(review.id);
+  };
   return (
     <Box
       sx={{
@@ -43,6 +58,23 @@ const ReviewItem = ({ review, onRemoved }) => {
           <Typography variant="body1" textAlign="justify">
             {review.content}
           </Typography>
+          {user && user.id === review.user.id && (
+            <LoadingButton
+              variant="contained"
+              startIcon={<DeleteIcon />}
+              loadingPosition="start"
+              loading={onRequest}
+              onClick={onRemove}
+              sx={{
+                position: { xs: "relative", md: "absolute" },
+                right: { xs: 0, md: "10px" },
+                marginTop: { xs: 2, md: 0 },
+                width: "max-content",
+              }}
+            >
+              remove
+            </LoadingButton>
+          )}
         </Stack>
       </Stack>
     </Box>
@@ -56,6 +88,8 @@ const MediaReview = ({ reviews, media, mediaType }) => {
   const [page, setPage] = useState(1);
   // console.log(listReviews);
   const { user } = useSelector((state) => state.user);
+  const [content, setContent] = useState("");
+  const [onRequest, setOnRequest] = useState(false);
 
   const skip = 4;
 
@@ -72,13 +106,50 @@ const MediaReview = ({ reviews, media, mediaType }) => {
     ]);
     setPage(page + 1);
   };
+  const onAddReview = async () => {
+    if (onRequest) return;
+    setOnRequest(true);
+
+    const body = {
+      content,
+      mediaId: media.id,
+      mediaType,
+      mediaTitle: media.title || media.name,
+      mediaPoster: media.poster_path,
+    };
+    const { response, err } = await reviewApi.add(body);
+
+    setOnRequest(false);
+
+    if (err) toast.error(err.message);
+    if (response) {
+      toast.success("Post review success");
+      setFilteredReviews([...filteredReviews, response]);
+      setReviewCount(reviewCount + 1);
+      setContent("");
+    }
+  };
+
+  const onRemoved = (id) => {
+    if (listReviews.findIndex((e) => e.id === id) !== -1) {
+      const newListReviews = [...listReviews].filter((e) => e.id !== id);
+      setListReviews(newListReviews);
+      setFilteredReviews([...newListReviews].splice(0, page * skip));
+    } else {
+      setFilteredReviews([...filteredReviews].filter((e) => e.id !== id));
+    }
+
+    setReviewCount(reviewCount - 1);
+
+    toast.success("Remove review success");
+  };
   return (
     <>
       <Container header={`Reviews (${reviewCount})`}>
         <Stack>
           {filteredReviews.map((item) => (
             <Box>
-              <ReviewItem review={item} />
+              <ReviewItem review={item} onRemoved={onRemoved} />
               <Divider
                 sx={{
                   display: { xs: "block", md: "none" },
@@ -105,6 +176,8 @@ const MediaReview = ({ reviews, media, mediaType }) => {
                   rows={4}
                   placeholder="Write your review"
                   variant="outlined"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
                 />
                 <LoadingButton
                   variant="contained"
@@ -112,6 +185,8 @@ const MediaReview = ({ reviews, media, mediaType }) => {
                   sx={{ width: "max-content" }}
                   startIcon={<SendOutlinedIcon />}
                   loadingPosition="start"
+                  loading={onRequest}
+                  onClick={onAddReview}
                 >
                   post
                 </LoadingButton>
