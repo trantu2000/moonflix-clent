c2ab812b0dd051b3ec47b29e5f4829677154076c
tran thanh tu <tttuab@gmail.com>
2023-01-31 14:56:27 +0700

    update

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import Topbar from "../common/Topbar";
 import AuthModal from "../common/AuthModal";
 import Footer from "../common/Footer";
 import { Outlet } from "react-router-dom";
 import GlobalLoading from "../common/GlobalLoading";
+import { useDispatch, useSelector } from "react-redux";
+import userApi from "../../api/modules/user.api";
+import { setUser } from "../../redux/features/userSlice";
 
 const MainLayout = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userApi.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
   return (
     <>
       {/* globalLoading */}
